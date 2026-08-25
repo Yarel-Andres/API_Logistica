de8d116e00e7d1c9f074383a933027548f6c85e7
YarelAndres <andres.sanchez221@gmail.com>
2026-08-25 14:07:56 +0200

    Cambio de formato de import.sql para cargar correctamente a los repartidores

--- logistica-api/src/main/resources/import.sql
+++ logistica-api/src/main/resources/import.sql
@@ -1,8 +1,4 @@
-INSERT INTO repartidor (nombre, apellido, disponible)
-VALUES ('Carlos', 'Mendoza', true);
-INSERT INTO repartidor (nombre, apellido, disponible)
-VALUES ('Ricardo', 'Fernandez', true);
-INSERT INTO repartidor (nombre, apellido, disponible)
-VALUES ('Lorena', 'Gutierrez', true);
-INSERT INTO repartidor (nombre, apellido, disponible)
-VALUES ('Marcos', 'Berrocal', true);
+INSERT INTO repartidor (nombre, apellido, disponible) VALUES ('Carlos', 'Mendoza', true);
+INSERT INTO repartidor (nombre, apellido, disponible) VALUES ('Ricardo', 'Fernandez', true);
+INSERT INTO repartidor (nombre, apellido, disponible) VALUES ('Lorena', 'Gutierrez', true);
+INSERT INTO repartidor (nombre, apellido, disponible) VALUES ('Marcos', 'Berrocal', true);
